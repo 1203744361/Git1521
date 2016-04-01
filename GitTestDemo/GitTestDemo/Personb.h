c74dcd3051b41bda2a05bb6537e4078d808d60fb
1203744361 <>
2016-04-01 15:06:14 +0800

    B创建一个PersonB类

---
+++ GitTestDemo/GitTestDemo/Personb.h
@@ -0,0 +1,13 @@
+//
+//  Personb.h
+//  GitTestDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 ChenJian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Personb : NSObject
+
+@end
---
+++ GitTestDemo/GitTestDemo/Personb.m
@@ -0,0 +1,13 @@
+//
+//  Personb.m
+//  GitTestDemo
+//
+//  Created by qianfeng on 16/4/1.
+//  Copyright © 2016年 ChenJian. All rights reserved.
+//
+
+#import "Personb.h"
+
+@implementation Personb
+
+@end
